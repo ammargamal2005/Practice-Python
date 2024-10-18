dcdf5aa50b5bc27eb73b33b5f6f985b7b7fa0308
Ammar gamal <148873902+ammargamal2005@users.noreply.github.com>
2024-10-18 16:59:58 +0300

    Update math_function.py

--- math_function.py
+++ math_function.py
@@ -18,6 +18,28 @@ def min(num1,num2):
     
     else :
         return None 
+def abs(num):
+    if num>=0  :
+        return num
+    return -num
+          
+        
+def our_max(num1,num2):
+   num1=abs(num1)
+   num2=abs(num2)
+   
+   if num1>num2:
+        return num1
+    
+   elif num1<num2:
+        return num2
+    
+   else :
+        return None 
+def my_floar (num):
+    return int(num)
+def my_ciel (num):
+    return int(num)+1
 
-print( "max number is ",max(5,7))  #7
-print("max number is ",min(5,7))   #5
+
+
