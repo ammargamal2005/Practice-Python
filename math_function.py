08d4dc10cdc520aa303dc11c5e037d356ff1ffa6
Ammar gamal <148873902+ammargamal2005@users.noreply.github.com>
2024-10-19 02:18:18 +0300

    Update math_function.py

--- math_function.py
+++ math_function.py
@@ -5,10 +5,10 @@ def max(num1,num2):
     if num1>num2:
         return num1
     elif num2>num1 :
-        return num2
-    
+        return num2 
     else :
         return None 
+
 #min function
 def min(num1,num2):
     if num1<num2:
