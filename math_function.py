46a4b9a8c588207cf7d760012021fe526774b518
Ammar gamal <148873902+ammargamal2005@users.noreply.github.com>
2024-10-21 13:29:24 +0300

    Update math_function.py

--- math_function.py
+++ math_function.py
@@ -1,5 +1,4 @@
 #math founction
-
 #max function
 def max(num1,num2):
     if num1>num2:
@@ -8,14 +7,12 @@ def max(num1,num2):
         return num2 
     else :
         return None 
-
 #min function
 def min(num1,num2):
     if num1<num2:
         return num1
     elif num2<num1 :
-        return num2
-    
+        return num2   
     else :
         return None 
 def abs(num):
@@ -45,6 +42,22 @@ def my_ciel (num):
     num = abs(num)
     return int(num)+1
 
+def my_exp (x):
+    return 2.718282**x
 
 
+def my_factorial(x):
+    factor = 1
+    for x in range(1,x+1):
+        factor *=x
+    return factor    
 
+# The perimeter of any equilateral shape
+def Perimeter_of_shape (length,numOfShape):
+    return length*numOfShape   
+    
+    
+def circle (rad):
+    print( "Area of circle is", 3.14*(rad**2))
+    print ("Perimeter of circle is",2*3.1415*rad)
+    
